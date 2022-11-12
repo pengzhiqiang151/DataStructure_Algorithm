7aeacbc158fd9764441d0da8f2a04d593dfc0324
pengzq <pzqdbs@126.com>
2022-11-12 17:13:23 +0800

    add predict function in tree_cart.py

--- Algorithm/tree_cart.py
+++ Algorithm/tree_cart.py
@@ -10,7 +10,7 @@ class TreeNode:
 
     def __init__(self, dt=None):
         self.data = dt  # 当前节点数据
-        self.rule = None  # 分裂规则
+        self.rule = None  # 分裂规则    （特征的索引，阈值）
         self.left = None  # 左节点的指向
         self.right = None  # 右节点的指向
         self.predict = None  # 预测结果 (叶节点)
@@ -19,14 +19,15 @@ class TreeNode:
 class CartTree(object):
     """类：训练一棵树，预测树的结果"""
 
-    def __init__(self, depth: int):
+    def __init__(self, *, depth: int):
         self.depth = depth
         self.tree = None  # 初始化树的根节点
-        self.num_leaf = 0   # 记录叶节点的个数
+        self.num_leaf = 0  # 记录叶节点的个数
 
     @staticmethod
     def calculate_inf_gain(dt: pd.DataFrame, ind: int):
-        """计算当前特征当前分裂节点下的信息增益
+        """分类问题
+        计算当前特征当前分裂节点下的信息增益
         ind 为分裂节点的行级索引"""
         df_left = dt[dt.iloc[:, 1] <= dt.iloc[ind, 1]]
         df_right = dt[dt.iloc[:, 1] > dt.iloc[ind, 1]]
@@ -63,31 +64,31 @@ class CartTree(object):
     def train(self, dt: np.array, depth: int = 0):
         """创建树"""
         # 创建节点
-        node = TreeNode(dt)
+        node = TreeNode()
         # 终止条件: 深度超过设定值, 样本类别为 1, 节点样本数小于阈值
         depth += 1
         print('树的深度: {}'.format(depth))
         if depth > self.depth or sum(dt[:, 0] != dt[0, 0]) == 0 or dt.shape[0] <= 10:
-            node.predict = pd.DataFrame(node.data[:, 0]).mode().iloc[0, 0]  # 为叶节点赋值
+            node.predict = pd.DataFrame(dt[:, 0]).mode().iloc[0, 0]  # 为叶节点赋值
             self.num_leaf += 1
-            print('生成第 {} 个 叶节点'.format(self.num_leaf))
+            print('生成第 {} 个 叶节点, 输出值 {}'.format(self.num_leaf, node.predict))
             return node
         # 寻找当前节点的分裂规则(特征、分裂值) [ind, value]   (dt[ind] ? value)
         node.rule = self.find_split(dt)
         # 递归创建左子树和右子树
-        data_left = node.data[node.data[:, node.rule[0]] <= node.rule[1], :]
-        data_right = node.data[node.data[:, node.rule[0]] > node.rule[1], :]
+        data_left = dt[dt[:, node.rule[0]] <= node.rule[1], :]
+        data_right = dt[dt[:, node.rule[0]] > node.rule[1], :]
         if data_left.shape[0] == 0:  # 左节点样本数为空, 终止
-            node.left = TreeNode()      # 生成左叶节点
-            node.left.predict = pd.DataFrame(node.data[:, 0]).mode().iloc[0, 0]     # 为叶节点赋值
+            node.left = TreeNode()  # 生成左叶节点
+            node.left.predict = pd.DataFrame(dt[:, 0]).mode().iloc[0, 0]  # 为叶节点赋值
             self.num_leaf += 1
-            print('生成第 {} 个 叶节点'.format(self.num_leaf))
+            print('生成第 {} 个 叶节点, 输出值 {}'.format(self.num_leaf, node.left.predict))
             return
         if data_right.shape[0] == 0:  # 右节点样本数为空, 终止
-            node.right = TreeNode()     # 生成右叶节点
-            node.right.predict = pd.DataFrame(node.data[:, 0]).mode().iloc[0, 0]    # 为叶节点赋值
+            node.right = TreeNode()  # 生成右叶节点
+            node.right.predict = pd.DataFrame(dt[:, 0]).mode().iloc[0, 0]  # 为叶节点赋值
             self.num_leaf += 1
-            print('生成第 {} 个 叶节点'.format(self.num_leaf))
+            print('生成第 {} 个 叶节点, 输出值 {}'.format(self.num_leaf, node.right.predict))
             return
         node.left = self.train(data_left, depth)
         node.right = self.train(data_right, depth)
@@ -95,8 +96,24 @@ class CartTree(object):
         print('完成一个节点的训练')
         return node
 
-    def predict(self):
-        pass
+    def predict_sample(self, node: TreeNode, one_sample):
+        """遍历决策树, 找到叶子节点"""
+        # 终止条件
+        if node.predict is not None:
+            print('当前预测结果：{}'.format(node.predict))
+            return node.predict
+        # 查询左节点 (注：索引数比训练时少一个)
+        if one_sample[node.rule[0]-1] <= node.rule[1]:
+            return self.predict_sample(node.left, one_sample)
+        # 查询右节点
+        else:
+            return self.predict_sample(node.right, one_sample)
+
+    def predict(self, node: TreeNode, dt: np.array):
+        result = []
+        for sample in dt:
+            result.append(self.predict_sample(node, sample))
+        return result
 
 
 if __name__ == '__main__':
@@ -108,4 +125,13 @@ if __name__ == '__main__':
     data = np.hstack((y.reshape(-1, 1), x))
 
     model = CartTree(depth=2)
-    model.train(data)
+    tree_node = model.train(data)
+
+    y_predict = model.predict(tree_node, x)
+
+    num_acc = 0
+    for i in range(len(y_predict)):
+        if y_predict[i] == y[i]:
+            num_acc += 1
+    print('acc:', num_acc/len(y_predict))
+
